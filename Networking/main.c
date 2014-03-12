a7cc7b4d924f5d79d0c099e9103eb35a3139fc25
John Drogo <business@jldapps.com>
2014-03-12 17:47:36 -0400

    Initial Commit

---
+++ Networking/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Networking
+//
+//  Created by John Drogo on 3/12/14.
+//  Copyright (c) 2014 John Drogo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
